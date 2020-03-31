770091c62fd25d08e8b2088975fb50471f31ee8d
Ruiz Robinson <robinson.ruiz@cpe.fr>
2020-03-31 22:54:42 +0100

    plusieurs trucs pour tester : cycle jour-nuit, shader de terrain, personnage et skybox dynamique en cours

---
+++ Project/Assets/Lighting/Day_and_night_cycle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day_and_night_cycle : MonoBehaviour
+{
+    public float _speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.RotateAround(new Vector3(0, 0, 0), new Vector3(1, 0, 0), _speed);
+    }
+}
